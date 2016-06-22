3c2e391deaeb36c201ffbe249d6777ef715e1faf
daco88 <daniel.corbalan@outlook.com>
2016-06-22 11:27:54 -0300

    Cambios nivel
    A partir del primer modelo genere un nuevo nivel con fisicas propias.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,16 @@ public class PlayerController : MonoBehaviour {
 	public Text countText;
 	public Text winText;
 
-	private int count;
+    private int count;
 	private Rigidbody rb;
-	private Vector3 originalPos;
+    private Vector3 moveDir;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		rb = GetComponent<Rigidbody>();
 		count = 0;
 		SetCountText ();
 		winText.text = "";
-		originalPos = transform.position;
         if (SystemInfo.deviceType == DeviceType.Handheld)
         {
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
@@ -26,7 +25,17 @@ public class PlayerController : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	// void Update () {}
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.LeftControl) || Input.touchCount==1)
+        {
+            Stop();
+        }
+    }
 
 	// Despues de los calculos de la fisica del objeto
 	void FixedUpdate()
@@ -34,38 +43,33 @@ public class PlayerController : MonoBehaviour {
         //Movimientos en PC
 		if (SystemInfo.deviceType == DeviceType.Desktop) 
 		{
-			float y =  0.0f;
-			Vector3 pos = transform.position;
-			if (pos.y >= 0.0f) {
-				if (Input.GetKey (KeyCode.Space) && pos.y <= 0.8f) {
-					y = 8.0f;
-				}
+            float y = 0.0f;
 
-				float moveHorizontal = Input.GetAxis ("Horizontal");
-				float moveVertical = Input.GetAxis ("Vertical");
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            float moveVertical = Input.GetAxisRaw("Vertical");
 
-				Vector3 movement = new Vector3 (moveHorizontal, y, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, y, moveVertical);
 
-				rb.AddForce (movement * speed);
-			} else {
-				transform.position = originalPos;
-				Stop ();
-			}
-		}
+            rb.AddForce(transform.TransformDirection(movement) * speed);
+            
+            
+            
+        }
         // Movimiento en dispositivos mobile
         else
         {
-            float moveH = Input.acceleration.x;
-            float moveV = Input.acceleration.y;
+            float moveH = Input.acceleration.x * 3;
+            float moveV = Input.acceleration.y * 3;
 
             Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+            
+            rb.AddForce(transform.TransformDirection(movement) * speed * Time.deltaTime);
 
-            rb.AddForce(movement * speed * Time.deltaTime);            
         }
 	}
 
 	void Stop(){
-		rb.velocity = Vector3.zero;
+		rb.velocity = rb.velocity - rb.velocity/10;
 		rb.angularVelocity = Vector3.zero;
 	}
 
@@ -76,15 +80,6 @@ public class PlayerController : MonoBehaviour {
 			other.gameObject.SetActive (false);
 			count = count + 1;
 			SetCountText ();
-		}
-	}
-
-	void Update() {
-		if (Input.GetKey (KeyCode.Escape)) {
-			Application.Quit ();
-		}
-		if (Input.GetKey (KeyCode.LeftControl)) {
-			Stop ();
 		}
 	}
 
