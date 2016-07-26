45ba3c0eec2b89a08a8d18458a335e2606060535
fserron <federico.serron@gmail.com>
2016-07-26 01:22:09 -0300

    Addendum
    Este quedo suelto, vaya a saber uno porque

--- Assets/Scripts/System/mensajeInicio.cs
+++ Assets/Scripts/System/mensajeInicio.cs
@@ -2,23 +2,39 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class mensajeInicio : MonoBehaviour {
+public class MensajeInicio : MonoBehaviour {
 
 	public Text objetivoTiempo;
 	public Text objetivoCubos;
 
-	void Start () {
+	public void mostrarObjetivos(Objetivo[] objetivos){
 		objetivoTiempo = objetivoTiempo.GetComponent<Text>();
 		objetivoCubos = objetivoCubos.GetComponent<Text>();
-		objetivoCubos.text = "Juntar " + " cubos";
-		objetivoTiempo.text = "en " + " segundos";
-		StartCoroutine(Mensaje());
+		int puntos = 0;
+		float tiempo = 0.0f;
+
+		//Buscamos los objetivos
+		foreach (Objetivo o in objetivos)
+		{
+			if (o is ObjetivoTiempo)
+			{
+				tiempo = ((ObjetivoTiempo) o).getTiempoObjetivo();
+			}
+			if (o is ObjetivoPuntos) 
+			{
+				puntos = ((ObjetivoPuntos)o).getPuntosObjetivo();
+			}
+		}
+
+		objetivoCubos.text = "Juntar " + puntos + " cubos";
+		objetivoTiempo.text = "en " + tiempo + " segundos";
+		StartCoroutine(Mensaje(3)); //Muestra el mensaje por 3 segundos
 	}
 	
-	IEnumerator Mensaje() {
+	IEnumerator Mensaje(int segundos) {
 		objetivoCubos.enabled = true; // Muestra el texto
 		objetivoTiempo.enabled = true;
-		yield return new WaitForSeconds(3); //Espera 3 segundos
+		yield return new WaitForSeconds(segundos); //Espera X segundos
 		objetivoCubos.enabled = false; // Esconde el texto
 		objetivoTiempo.enabled = false;
 	}
